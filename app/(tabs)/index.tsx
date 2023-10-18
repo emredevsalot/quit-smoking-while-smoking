a1edaa050f862e0e5132ec9aaf89c8620a8f5ecd
Emre Ergül <emredevsalot@gmail.com>
2023-10-18 13:44:22 +0300

    add smoke data tracking

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,173 @@
-import { StyleSheet } from 'react-native';
+// TODO: Button will come from Themed components as well.
+import { useState, useEffect } from 'react';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import moment from 'moment';
+
+import { StyleSheet, Button } from 'react-native';
 import { Text, View } from '../../components/Themed';
 
+// // WEB_NOTIFICATION_ERROR
+// import * as Notifications from 'expo-notifications';
+
 export default function TabOneScreen() {
+  const INITIAL_COOLDOWN_MINUTES = 35;
+  const [habitData, setHabitData] = useState({
+    lastSmokeTime: new Date().getTime(),
+    cooldownMinutes: INITIAL_COOLDOWN_MINUTES,
+    smokeCount: 0,
+  });
+  const [timeLeft, setTimeLeft] = useState(habitData.cooldownMinutes * 60 * 1000);
+  const [isCooldownCompleted, setIsCooldownCompleted] = useState(false);
+  // TESTING:
+  console.log('timeleft: ', timeLeft);
+
+  // Function to update the timer
+  const updateTimer = () => {
+    if (habitData.lastSmokeTime) {
+      const currentTime = new Date().getTime();
+      const elapsedTime = currentTime - habitData.lastSmokeTime;
+      const remainingTime = habitData.cooldownMinutes * 60 * 1000 - elapsedTime;
+
+      setTimeLeft(Math.abs(remainingTime));
+
+      if (remainingTime < 0) {
+        setIsCooldownCompleted(true);
+      } else {
+        setIsCooldownCompleted(false);
+      }
+    } else {
+      // FIX: This part is executed if the user hasn't smoked yet or resetted the habit data.
+      // We hardcode 2 seconds now. If we don't do this, timeLeft stays where it was
+      // before resetting the habit data. We can do something else here.
+      setTimeLeft(2000);
+    }
+  };
+
+  // Update the timer every second
+  useEffect(() => {
+    const timerInterval = setInterval(updateTimer, 1000);
+    return () => clearInterval(timerInterval); // Cleanup on unmount
+  }, [habitData]);
+
+  // Load habit data from AsyncStorage
+  const loadHabitData = async () => {
+    try {
+      const storedData = await AsyncStorage.getItem('habitData');
+      if (storedData) {
+        setHabitData(JSON.parse(storedData));
+      }
+    } catch (error) {
+      console.error('Error loading habit data:', error);
+    }
+  };
+
+  // Request permission to send notifications (if not already granted).
+  // // WEB_NOTIFICATION_ERROR
+  // const getNotificationPermission = async () => {
+  //   const { status } = await Notifications.requestPermissionsAsync();
+  //   if (status !== 'granted') {
+  //     console.warn('Permission to receive notifications was denied.');
+  //   }
+  // };
+
+  // Load habit data and check permissions when the component mounts.
+  useEffect(() => {
+    loadHabitData();
+    // // WEB_NOTIFICATION_ERROR
+    // getNotificationPermission();
+  }, []);
+
+  const handleResetButtonClick = async () => {
+    // Reset habit data to initial state.
+    const initialHabitData = {
+      lastSmokeTime: new Date().getTime(),
+      cooldownMinutes: INITIAL_COOLDOWN_MINUTES,
+      smokeCount: 0,
+    };
+
+    setHabitData(initialHabitData);
+
+    // Save the initial state to AsyncStorage.
+    await AsyncStorage.setItem('habitData', JSON.stringify(initialHabitData));
+  };
+
+  const handleSmokeButtonClick = () => {
+    const currentTime = new Date().getTime();
+    const lastSmokeDate = new Date(habitData.lastSmokeTime);
+    const currentSmokeDate = new Date(currentTime);
+
+    const newHabitData = {
+      ...habitData,
+      smokeCount: habitData.smokeCount + 1,
+      lastSmokeTime: currentTime,
+    };
+
+    // Increase cooldown by 1 minute if a new day
+    // has started since the last smoke
+    if (
+      lastSmokeDate.getFullYear() !== currentSmokeDate.getFullYear() ||
+      lastSmokeDate.getMonth() !== currentSmokeDate.getMonth() ||
+      lastSmokeDate.getDate() !== currentSmokeDate.getDate()
+    ) {
+      newHabitData.cooldownMinutes += 1;
+    }
+    newHabitData.cooldownMinutes += 1;
+
+    setHabitData(newHabitData);
+
+    // Save updated habit data to AsyncStorage.
+    AsyncStorage.setItem('habitData', JSON.stringify(newHabitData));
+
+    // Schedule a notification for after the cooldown.
+    // // WEB_NOTIFICATION_ERROR
+    // const trigger = new Date(newHabitData.lastSmokeTime + newHabitData.cooldownMinutes * 60 * 1000);
+    // scheduleNotification(trigger);
+  };
+
+  // Function to schedule a notification
+  // // WEB_NOTIFICATION_ERROR
+  // const scheduleNotification = (trigger: Date) => {
+  //   console.log('Set notification for:', moment(trigger.getTime()).format('hh:mm:ss a'));
+
+  //   Notifications.setNotificationHandler({
+  //     handleNotification: async () => ({
+  //       shouldShowAlert: true,
+  //       shouldPlaySound: true,
+  //       shouldSetBadge: true,
+  //     }),
+  //   });
+
+  //   Notifications.scheduleNotificationAsync({
+  //     content: {
+  //       title: 'Time to Smoke Again!!',
+  //       body: 'You can smoke now.',
+  //     },
+  //     trigger,
+  //   });
+  // };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>
+        {isCooldownCompleted ? 'Going extra for: ' : 'Time left: '}
+        {moment.utc(timeLeft).format('HH:mm:ss')}
+      </Text>
+      <Text>Cooldown: {habitData.cooldownMinutes} minutes</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text>Smoke count: {habitData.smokeCount}</Text>
+      <Text>
+        Last smoked:{' '}
+        {habitData.lastSmokeTime && moment(habitData.lastSmokeTime).format('hh:mm:ss a')}
+      </Text>
+      <Text>
+        Next cigarette at:{' '}
+        {moment(habitData.lastSmokeTime + habitData.cooldownMinutes * 60 * 1000).format(
+          'hh:mm:ss a',
+        )}
+      </Text>
+      <Button title="Smoke" onPress={handleSmokeButtonClick} />
+      <Button title="Reset Habit Data" onPress={handleResetButtonClick} />
     </View>
   );
 }
@@ -16,6 +175,7 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    gap: 16,
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -24,7 +184,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   separator: {
-    marginVertical: 30,
+    marginVertical: 1,
     height: 1,
     width: '80%',
   },
